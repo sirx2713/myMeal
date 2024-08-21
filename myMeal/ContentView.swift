fe49ba4f590bb8e2fc68a716d39b1335bec8d8d7
iSAD <bradx2713@gmail.com>
2024-08-21 09:36:40 +0200

    Initial Commit

---
+++ myMeal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myMeal
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myMeal/myMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  myMealApp.swift
+//  myMeal
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/08/21.
+//
+
+import SwiftUI
+
+@main
+struct myMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
